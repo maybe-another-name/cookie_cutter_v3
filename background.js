f246879b900976531a2a399e77ac97f20b1bf519
maybe-another-name <email.yet.another.one@gmail.com>
2021-05-11 10:57:19 -0400

    all operations complete

--- background.js
+++ background.js
@@ -14,33 +14,60 @@ function showSplash(info, tab) {
 }
 
 // react to messages from the extension
-chrome.runtime.onMessage.addListener((json_message, chrome_runtime, function_callback) => {
-  try {
-    json_message = JSON.parse(json_message);
-    console.log("received message");
-    console.log(json_message);
-  } catch (v) {
-    json_message = {};
-  }
+chrome.runtime.onMessage.addListener(
+  (json_message, chrome_runtime, function_callback) => {
+    try {
+      json_message = JSON.parse(json_message);
+      console.log("received message");
+      console.log(json_message);
+    } catch (v) {
+      json_message = {};
+    }
 
-  switch (json_message.id) {
-    case 1:
-      function_callback();
-      break;
-    case 3:
-      apply_function_to_cookies(function_callback);
-      break;
-    case 5:
-      break;
-    case 7:
-      break;
+    switch (json_message.id) {
+      case 1:
+        function_callback();
+        break;
+      case 3:
+        apply_function_to_cookies(function_callback);
+        break;
+      case 5:
+        remove_cookies_by_attribute(json_message.v || []);
+        function_callback();
+        break;
+      case 7:
+        remove_cookies(json_message.v || []);
+        function_callback();
+        break;
+    }
+    return true;
   }
-  return true;
-});
+);
 
 apply_function_to_cookies = function (function_to_apply) {
   chrome.cookies.getAll({}, function (e) {
     log_last_error() ? function_to_apply(null) : function_to_apply(e);
+  });
+};
+
+remove_cookies = function (a) {
+  a.forEach((a) => {
+    chrome.cookies.remove(a, log_last_error);
+  });
+};
+
+remove_cookies_by_attribute = function (a) {
+  a.forEach((a) => {
+    chrome.cookies.remove(
+      {
+        url: a.url,
+        name: a.name,
+      },
+      function () {
+        log_last_error();
+        chrome.cookies.set(a, log_last_error);
+      }
+    );
   });
 };
 
